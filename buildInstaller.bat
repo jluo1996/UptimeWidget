b965f3c29c5b0e819ea279c8cd0a43346e5cea97
Jianliang Luo <jayluo1996@gmail.com>
2026-07-25 18:22:49 -0700

    fix installer build

--- buildInstaller.bat
+++ buildInstaller.bat
@@ -5,7 +5,8 @@ set "CONFIG=%~1"
 if "%CONFIG%"=="" set "CONFIG=Release"
 
 set "PAYLOAD_DIR=%~2"
-if "%PAYLOAD_DIR%"=="" set "PAYLOAD_DIR=%~dp0UptimeWidget\UptimeWidget\bin\x64\Release\net10.0-windows"
+if "%PAYLOAD_DIR%"=="" set "PAYLOAD_DIR=%~dp0UptimeWidget\UptimeWidget\bin\x64\Release\net10.0-windows\"
+if "%PAYLOAD_DIR:~-1%"=="\" set "PAYLOAD_DIR=%PAYLOAD_DIR:~0,-1%"
 
 echo Building UptimeWidget installer with configuration: %CONFIG%
 echo Payload directory: %PAYLOAD_DIR%
