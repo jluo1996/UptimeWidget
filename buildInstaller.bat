13ff5f38c56d55606c272b5abaf1930e557e4c19
Jianliang Luo <jayluo1996@gmail.com>
2026-07-25 18:20:09 -0700

    add echo

--- buildInstaller.bat
+++ buildInstaller.bat
@@ -7,6 +7,9 @@ if "%CONFIG%"=="" set "CONFIG=Release"
 set "PAYLOAD_DIR=%~2"
 if "%PAYLOAD_DIR%"=="" set "PAYLOAD_DIR=%~dp0UptimeWidget\UptimeWidget\bin\x64\Release\net10.0-windows"
 
+echo Building UptimeWidget installer with configuration: %CONFIG%
+echo Payload directory: %PAYLOAD_DIR%
+
 dotnet build "UptimeWidgetInstaller\UptimeWidgetInstaller\UptimeWidgetInstaller.wixproj" -c "%CONFIG%" -p:PayloadDir="%PAYLOAD_DIR%"
 
 if errorlevel 1 exit /b %errorlevel%
